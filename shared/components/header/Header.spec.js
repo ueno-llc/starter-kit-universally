6e3112ab8d81296dae0ae936e710ddec6e5feaae
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-02-18 04:20:14 +0000

    Added basic tests

---
+++ shared/components/header/Header.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
+import UenoLogoSvg from 'assets/images/ueno-logo.svg';
+import Header from 'components/header';
+
+it('contains the Ueno. logo', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find(UenoLogoSvg).length).toEqual(1);
+});
+
+it('contains link to home', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find(Link).prop('to')).toEqual('/');
+});
