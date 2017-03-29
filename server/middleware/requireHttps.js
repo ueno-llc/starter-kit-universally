191004e461aefe65f5839e0da226cb4bd07d378a
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2017-03-29 17:52:51 +0000

    Add redirect to https

---
+++ server/middleware/requireHttps.js
@@ -0,0 +1,7 @@
+
+export default function requireHttpsMiddleware(req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(301, 'https://' + req.get('host') + req.url);
+  }
+  next();
+}
