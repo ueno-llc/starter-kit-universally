7a4f2f36b847cf3b50ffa28371aebbb8ef72006e
Hrafn Einarsson <hrafn.einarsson@gmail.com>
2017-06-14 15:24:03 +0000

    ability to toggle the devtools :wrench:

--- shared/components/devtools/DevTools.js
+++ shared/components/devtools/DevTools.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import autobind from 'core-decorators/lib/autobind';
 
 const showDevTools = process.env.BUILD_FLAG_IS_DEV === 'true';
 const MobxDevTools = showDevTools && require('mobx-react-devtools').default;
 const GridOverlay = showDevTools && require('components/grid-overlay').default;
 
-const DevTools = showDevTools ? () => (
-  <div>
-    <MobxDevTools />
-    <GridOverlay columns={12} baseline={16} />
-  </div>
-) : (() => null);
+const LOCAL_STORAGE_KEY_VISIBLE = '_devtoolsVisible';
 
-export default DevTools;
+@observer
+class DevTools extends Component {
+  @observable display = false;
+
+  componentDidMount() {
+    this.display = (localStorage.getItem(LOCAL_STORAGE_KEY_VISIBLE) === 'true');
+
+    document.addEventListener('keydown', this.keydownRef = this.onKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.keydownRef);
+  }
+
+  @autobind
+  onKeyDown(e) {
+    if (e.ctrlKey && e.keyCode === 75) {
+      this.onToggleDisplay();
+    }
+  }
+
+  @autobind
+  onToggleDisplay() {
+    this.display = !this.display;
+
+    localStorage.setItem(LOCAL_STORAGE_KEY_VISIBLE, this.display);
+  }
+
+  render() {
+    return (
+      <div>
+        <MobxDevTools noPanel={!this.display} />
+        <GridOverlay noPanel={!this.display} columns={12} baseline={16} />
+      </div>
+    );
+  }
+}
+
+export default showDevTools ? DevTools : (() => null);
