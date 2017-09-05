3793dcf345fdc9624801ea4a5170f6f146349201
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2017-09-05 15:42:13 +0000

    Refactor decorators so hot reloading works for planet components

--- shared/routes/planets/details/PlanetsDetail.js
+++ shared/routes/planets/details/PlanetsDetail.js
@@ -9,13 +9,8 @@ import Segment from 'components/segment';
 
 import RelatedPlanets from './components/related-planets';
 
-@inject(['planets'])
-@withJob({
-  work: ({ match, planets }) => planets.fetchById(match.params.id),
-  shouldWorkAgain: (prev, next) => prev.match.params.id !== next.match.params.id,
-})
-export default class PlanetsDetail extends Component {
-
+// can't decorate this class, it contains <RelatedPlanets> that has state
+class PlanetsDetail extends Component {
   static propTypes = {
     jobResult: PropTypes.shape({
       results: PropTypes.array,
@@ -55,3 +50,9 @@ export default class PlanetsDetail extends Component {
     );
   }
 }
+
+const planetsDetailWithJob = withJob({
+  work: ({ match, planets }) => planets.fetchById(match.params.id),
+  shouldWorkAgain: (prev, next) => prev.match.params.id !== next.match.params.id,
+})(PlanetsDetail);
+export default inject('planets')(planetsDetailWithJob);
