c8b226ebd047d3ebd3ed968fafda081391ca8a0a
Elisabeth Boonin <boon@ueno.co>
2017-09-15 18:41:07 -0700

    Generate html for hard-coded routes

--- internal/scripts/buildStatic.js
+++ internal/scripts/buildStatic.js
@@ -1,7 +1,31 @@
-import { removeSync, copySync } from 'fs-extra';
+import { removeSync, copySync, writeFileSync } from 'fs-extra';
+import superagent from 'superagent';
+import _ from 'lodash';
+import server from '../../build/server';
 
-const destinationDir = './build/static';
-const prebuiltClientDir = './build/client';
+const destinationDir = './build/static/';
+const builtClientBundles = './build/client/';
+
+const routes = ['', 'grid', 'planets', 'about'];
 
 removeSync(destinationDir);
-copySync(prebuiltClientDir, destinationDir);
+copySync(builtClientBundles, `${destinationDir}/client`);
+
+
+function renderRoute(routeName) {
+  const fileName = routeName.length > 0 ? `${routeName}.html` : 'index.html';
+  return superagent
+    .get(`http://localhost:3000/${routeName}`)
+    .then((res) => {
+      writeFileSync(`${destinationDir}/${fileName}`, res.text);
+    });
+}
+
+server.on('listening', () => {
+  console.log('Server is running, start to call html routes')
+  const promises = _.map(routes, renderRoute);
+  Promise.all(promises).then(() => {
+    server.close();
+    console.log(`static site generated in ${destinationDir}`);
+  });
+});
