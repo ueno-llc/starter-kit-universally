b7bb8f3f6ab2846ef25f5b3e08081b58f53c5bc3
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2017-09-27 11:53:25 +0000

    Update enzyme tests to run

--- shared/components/header/Header.spec.js
+++ shared/components/header/Header.spec.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
 
-import UenoLogoSvg from 'assets/images/ueno-logo.svg';
-
 import Header from 'components/header';
 
-it('contains the Ueno. logo', () => {
+it('contains a <header>', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper.find(UenoLogoSvg).length).toEqual(1);
+  expect(wrapper.find('header').length).toEqual(1);
 });
 
 it('contains link to home', () => {
