9436d7b833db2ab378fa688e252c4fd32ba6851a
Baldur Helgason <baldur.helgason@gmail.com>
2017-11-06 16:32:28 +0000

    Fixes bug: was returning the cli argument key and not the value

--- config/utils/cliVar.js
+++ config/utils/cliVar.js
@@ -2,14 +2,13 @@
  * Helper for resolving args passed via cli.
  */
 export default function cliVarString(name, defaultVal) {
-  let val = defaultVal;
   const args = process.argv.slice(2);
 
   const index = args.findIndex(arg => arg.includes(`--${name}`));
 
   if (index > -1) {
-    [val] = args[index].split('=');
+    return args[index].split('=')[1];
   }
 
-  return val;
+  return defaultVal;
 }
