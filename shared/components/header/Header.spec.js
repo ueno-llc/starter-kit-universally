b684f52017474b39ba4ee51e46abcc3446200169
Baldur Helgason <baldur.helgason@gmail.com>
2017-11-16 15:03:20 +0000

    Change import again

--- shared/components/header/Header.spec.js
+++ shared/components/header/Header.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Link } from 'react-router-dom';
 
-import Header from './Header';
+import Header from 'components/header';
 
 it('contains a <header>', () => {
   const wrapper = shallow(<Header />);
