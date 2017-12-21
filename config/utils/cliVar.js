e5de933ccd535298c90fcd3db18db16560c4cd43
Baldur Helgason <baldur.helgason@gmail.com>
2017-12-21 15:41:40 +0000

    Adds support for passing boolean flags with no value

--- config/utils/cliVar.js
+++ config/utils/cliVar.js
@@ -3,11 +3,20 @@
  */
 export default function cliVarString(name, defaultVal) {
   const args = process.argv.slice(2);
-
   const index = args.findIndex(arg => arg.includes(`--${name}`));
 
   if (index > -1) {
-    return args[index].split('=')[1];
+    const [key, val] = args[index].split('=');
+
+    if (val) {
+      return val;
+    }
+
+    if (key) {
+      // The option was passed but no value given
+      // User must be turning on a flag
+      return true;
+    }
   }
 
   return defaultVal;
