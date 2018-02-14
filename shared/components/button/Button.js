78e6842713dac990dd2977f15600907f49b69627
Jérémy Barbet <jeremgraph@gmail.com>
2018-02-14 10:21:27 +0000

    Remove unused props

--- shared/components/button/Button.js
+++ shared/components/button/Button.js
@@ -12,10 +12,6 @@ export default class Button extends PureComponent {
 
   static propTypes = {
     to: PropTypes.string,
-    alt: PropTypes.bool,
-    flat: PropTypes.bool,
-    large: PropTypes.bool,
-    small: PropTypes.bool,
     disabled: PropTypes.bool,
     children: PropTypes.node,
     className: PropTypes.string,
@@ -24,10 +20,6 @@ export default class Button extends PureComponent {
   render() {
     const {
       to,
-      alt,
-      flat,
-      large,
-      small,
       children,
       className,
       disabled,
@@ -39,23 +31,16 @@ export default class Button extends PureComponent {
     const isExternal = isLink && /^((https?:)?\/\/|[0-9a-zA-Z]+:)/.test(to);
 
     // Extend className of the rest
-    rest.className = s(s.button, className, {
-      alt,
-      flat,
-      large,
-      small,
-      disabled,
-    });
-
+    rest.className = s(s.button, className, { disabled });
     rest.disabled = disabled;
 
+    // http, https, //, mailto, etc.
     if (isExternal) {
-      // http, https, //, mailto, etc.
       return <a target="_blank" rel="noopener noreferrer" href={to} {...rest}>{children}</a>;
     }
 
+    // Everything else
     if (isLink) {
-      // Everything else
       return <Link to={to} {...rest}>{children}</Link>;
     }
 
